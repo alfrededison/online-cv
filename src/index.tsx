f46e74634f42f93c9d0f35a66fea1b6f142c527e
Hieu Nguyen <isaactesla@gmail.com>
2019-12-26 18:27:28 +0700

    Using react snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-//@ts-ignore
-import {render} from 'react-snapshot';
 import './index.scss';
+import React from 'react';
+import {hydrate, render} from "react-dom";
 import App from './components/Main/App';
 import * as serviceWorker from './serviceWorker';
 
-render(<App/>, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
