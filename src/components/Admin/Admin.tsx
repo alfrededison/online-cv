b1d377c6ee439e8174f977bc41c0740c05c2da15
Hieu Nguyen <isaactesla@gmail.com>
2019-12-31 15:43:32 +0700

    add admin theme basic

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -2,22 +2,32 @@
 import './Admin.scss';
 import * as React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
+import {themeConfig} from './utils/theme';
+
 import {NavBar} from "./fragments/NavBar";
 import {Home} from "./pages/Home";
 import {Register} from "./pages/Register";
 import {Login} from "./pages/Login";
 
+const theme = createMuiTheme(themeConfig);
+
 export const Admin = () => {
   return (
-    <BrowserRouter>
-      <NavBar/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/manage" component={Home}/>
-          <Route exact path="/manage/login" component={Login}/>
-          <Route exact path="/manage/register" component={Register}/>
-        </Switch>
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <BrowserRouter>
+          <NavBar/>
+          <div className="container">
+            <Switch>
+              <Route exact path="/manage" component={Home}/>
+              <Route exact path="/manage/login" component={Login}/>
+              <Route exact path="/manage/register" component={Register}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
-    </BrowserRouter>
+    </MuiThemeProvider>
   );
 };
---
+++ src/components/Admin/utils/theme.ts
@@ -0,0 +1,12 @@
+import {ThemeOptions} from "@material-ui/core/styles/createMuiTheme";
+
+export const themeConfig: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#1e88e5',
+    },
+    secondary: {
+      main: '#00c853',
+    },
+  },
+};
